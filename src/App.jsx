a52b8d2b4c3cdeed8902321955cc83b4baba63d6
Munshi91 <mimunshi91@gmail.com>
2024-06-09 18:41:16 +0600

    Bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Bookmarks from './components/Bookmarks/Bookmarks';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold ">knowledge Cafe</h1>
       <Header />
+      <div className='md:flex '>
+        <Blogs />
+        <Bookmarks />
+      </div>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/images/profile.png';
 const Header = () => {
   return (
-    <div>
-      <h2>Header File</h2>
+    <div className="flex justify-between items-center max-w-7xl mx-auto p-4  border-b-2">
+      <h2 className="text-4xl font-bold">Knowledge Cafe</h2>
+      <img src={profile} alt="" />
     </div>
   );
 };
