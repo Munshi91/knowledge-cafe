61745ee3a089f7671d0f67e27dab303302cf1c0f
Munshi91 <mimunshi91@gmail.com>
2024-06-09 16:48:03 +0600

    Api Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold ">knowledge Cafe</h1>
+      <Header />
     </>
   );
 }
